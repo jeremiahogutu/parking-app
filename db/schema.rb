98e09595af7605878f3b76774c9a93f26ea7ef84
Jeremiah Ogutu <jeremiahogutu@protonmail.com>
2018-08-20 07:32:05 -0400

    Added migrations for parking table

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180820113030) do
+
+  create_table "parkings", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "license"
+    t.decimal "price", precision: 10
+    t.integer "validity"
+    t.boolean "is_void"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
